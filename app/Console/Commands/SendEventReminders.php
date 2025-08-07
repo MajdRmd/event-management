cac2d65f034aa14930bedb7444bc970290adca21
MajdRmd <majdramadan7@gmail.com>
2025-08-07 16:46:42 +0300

    Notifications sent via mail (mailpit)

--- app/Console/Commands/SendEventReminders.php
+++ app/Console/Commands/SendEventReminders.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Event;
+use App\Notifications\EventReminderNotification;
 use Illuminate\Support\Str;
 use Illuminate\Console\Command;
 
@@ -35,7 +36,9 @@ class SendEventReminders extends Command
         $this->info("Found {$eventCount} {$eventLabel}");
         $events->each(
             fn($event) =>$event->attendees->each(
-                fn ($attendee) =>$this->info("Notifying the user {$attendee->user->id}")
+                fn ($attendee) =>$attendee->user->notify(
+                    new EventReminderNotification($event)
+                )
                 )
             );
         $this->info('Reminder notifications sent successfully!');
